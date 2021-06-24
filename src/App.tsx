63e8a4b87eedf01fd3796cdc74ffd6b78f8f1a8f
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2021-06-24 18:26:37 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { IonApp } from '@ionic/react';
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -24,16 +22,7 @@ import './theme/variables.css';
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <Home />
   </IonApp>
 );
 
